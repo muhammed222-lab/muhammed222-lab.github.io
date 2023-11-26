ef9a6ad4c6bf600cc3a0cb412cc40f64ae43612b
Muhammed Olayemi <110257578+muhammed222-lab@users.noreply.github.com>
2023-11-26 08:27:53 +0100

    Add files via upload
    I uploaded files

---
+++ script.js
@@ -0,0 +1,65 @@
+// Maintain cookies with JavaScript
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let messageInput = document.getElementById('message');
+let imageSelect = document.getElementById('image');
+
+// Check if cookies exist and fill form if they do
+let nameCookie = document.cookie.match(/name=([^;]*)/);
+if (nameCookie) {
+  nameInput.value = nameCookie[1];
+}
+
+let city = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+]
+
